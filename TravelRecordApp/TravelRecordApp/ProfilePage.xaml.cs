ed9da5a23a1f1b84f8d4ac5c7666a76757c95795
sotikouk <sotikou@uth.gr>
2023-05-20 11:00:18 +0300

    profile page complete.

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,28 @@ namespace TravelRecordApp
         public ProfilePage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                var postTable = conn.Table<Post>().ToList();
+                var categories = (from p in postTable 
+                    orderby p.CategoryId select p.CategoryName).Distinct().ToList();
+                Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+                foreach (var category in categories)
+                {
+                    var count = (from post in postTable 
+                            where post.CategoryName == category
+                                select post).ToList().Count;
+                    categoriesCount.Add(category,count);
+                }
+
+                categoriesListView.ItemsSource = categoriesCount;
+                postCountLabel.Text = postTable.Count().ToString();
+            }
         }
     }
 }
